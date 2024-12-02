8234f77d247054f78ba12d13a9b5486d00269eba
pavlov-n <pavlovmykyta1@gmail.com>
2024-12-02 14:11:00 +0200

    linter fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -84,8 +84,10 @@ export default function Home() {
     },
     onError: (e) => {
       const errorMessage = e.data?.zodError?.fieldErrors.content;
-      if (errorMessage && errorMessage[0]) {
-        toast.error(errorMessage[0]);
+      if (errorMessage) {
+        if (errorMessage[0]) {
+          toast.error(errorMessage[0]);
+        }
       } else {
         toast.error("Failed to post(");
       }
