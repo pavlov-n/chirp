187c90b99d06f42cde9af9595201b12e61843237
pavlov-n <pavlovmykyta1@gmail.com>
2024-12-03 11:21:22 +0200

    CI added

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,34 +1,11 @@
-import { createServerSideHelpers } from "@trpc/react-query/server";
-import superjson from "superjson";
-import { db } from "~/server/db";
 import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
-import { appRouter } from "~/server/api/root";
 import { api } from "~/utils/api";
 import LoadingSpinner from "~/components/loading";
 import { PageLayout } from "~/components/layout";
-import Image from "next/image";
-import Feed from "~/components/Feed";
 import { generateSSGHelper } from "~/server/helpers/ssgHelper";
 import PostView from "~/components/postview";
 
-const ProfileFeed = (props: { userId: string }) => {
-  const { data, isLoading } = api.post.getPostByUserId.useQuery({
-    userId: props.userId,
-  });
-
-  if (isLoading)
-    return (
-      <div className="absolute left-0 top-0 flex h-screen w-screen items-center justify-center">
-        <LoadingSpinner />
-      </div>
-    );
-  if (!data || data.length === 0) return <div>User has not posted</div>;
-
-  return <ul className="">
-
-  </ul>
-};
 
 const SinglePostPage: NextPage<{ id: string }> = ({ id }) => {
   const { data, isLoading } = api.post.getById.useQuery({
